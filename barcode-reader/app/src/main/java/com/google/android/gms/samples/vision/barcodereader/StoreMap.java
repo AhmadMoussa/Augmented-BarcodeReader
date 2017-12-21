ec871423c8a4bfa325671fc4edd5f6ee328e6833
Ahmad Moussa <amm65@mail.aub.edu>
2017-12-21 21:29:19 +0200

    Added explanations in forms of comments

--- barcode-reader/app/src/main/java/com/google/android/gms/samples/vision/barcodereader/StoreMap.java
+++ barcode-reader/app/src/main/java/com/google/android/gms/samples/vision/barcodereader/StoreMap.java
@@ -16,7 +16,7 @@ import com.google.android.gms.vision.barcode.Barcode;
 import java.util.ArrayList;
 
 /**
- * Created by christinebaertl on 17.12.17.
+ * Created by Ahmad Moussa on 17.12.17.
  */
 
 public class StoreMap extends Activity {
@@ -32,32 +32,31 @@ public class StoreMap extends Activity {
 
         RelativeLayout rl = (RelativeLayout) findViewById(R.id.map);
 
-        if(codes.size() == 1){
-            addItem1(rl, codes.get(0));
-        }else if(codes.size() == 2){
-            addItem1(rl, codes.get(0));
-            addItem2(rl, codes.get(1));
-        }else if(codes.size() == 3){
-            addItem1(rl, codes.get(0));
-            addItem2(rl, codes.get(1));
-            addItem3(rl, codes.get(2));
+        //loop over the array of codes that has been passed as intent from the main view
+        //each iteration will create a clickable circle to display an item on the map
+        for (int i = 0; i < codes.size(); i++) {
+            addItem(rl, codes.get(i));
         }
     }
 
-    private void addItem1(RelativeLayout rl, Code code){
+    private void addItem(RelativeLayout rl, Code code) {
+
+        //creating a new image View and encapsulating it in a Relative Layout
         ImageView iv;
         RelativeLayout.LayoutParams params;
         iv = new ImageView(this);
+
+        //Adding the drawable that is the image
         iv.setBackground(getDrawable(R.drawable.ic_add_circle_outline_black_24px));
         params = new RelativeLayout.LayoutParams(90, 90);
 
-        //System.out.println(codes.get(i).getX() + "");
-        //int x = Integer.parseInt(codes.get(i).getX());
-        params.leftMargin = 280;
+        //Get the X and Y coordinates of the item from the code object to set 
+        //the top and left margin such as to locate them correctly on the map
+        params.leftMargin = Integer.parseInt(code.getX());
+        params.topMargin = Integer.parseInt(code.getY());
 
-        //System.out.println(codes.get(i).getX() + "");
-        params.topMargin = 400;
-
+        //making a copy of the code object to be able to set an OnClickListener for the imageView
+        //which will show a popup once the circle is pressed
         final Code finalCode = new Code(code);
         iv.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,72 +64,12 @@ public class StoreMap extends Activity {
                 showPopup(v, 0, 0, finalCode);
             }
         });
+
+        //attaching the image view to the parent layout
         rl.addView(iv, params);
     }
 
-    private void addItem2(RelativeLayout rl, Code code){
-        ImageView iv;
-        RelativeLayout.LayoutParams params;
-        iv = new ImageView(this);
-        iv.setBackground(getDrawable(R.drawable.ic_add_circle_outline_black_24px));
-        params = new RelativeLayout.LayoutParams(90, 90);
-
-        //System.out.println(codes.get(i).getX() + "");
-        //int x = Integer.parseInt(codes.get(i).getX());
-        params.leftMargin = 730;
-
-        //System.out.println(codes.get(i).getX() + "");
-        params.topMargin = 100;
-
-        final Code finalCode = new Code(code);
-        iv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showPopup(v, 0, 0, finalCode);
-            }
-        });
-        rl.addView(iv, params);
-    }
-
-    //When you scan a barcode that is already in your cart,
-    //It will automatically color the list entry green,
-    //Such as to tell you that the item has been added to your cart
-
-    private void addItem3(RelativeLayout rl, Code code){
-        ImageView iv;
-        RelativeLayout.LayoutParams params;
-        iv = new ImageView(this);
-        iv.setBackground(getDrawable(R.drawable.ic_add_circle_outline_black_24px));
-        params = new RelativeLayout.LayoutParams(90, 90);
-
-        //System.out.println(codes.get(i).getX() + "");
-        //int x = Integer.parseInt(codes.get(i).getX());
-        params.leftMargin = 790;
-
-        //System.out.println(codes.get(i).getX() + "");
-        params.topMargin = 500;
-
-        final Code finalCode = new Code(code);
-        iv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showPopup(v, 0, 0, finalCode);
-            }
-        });
-        rl.addView(iv, params);
-    }
-
-    // The app can also help the user find a route to the store.
-
-    // The map will display markers on the location of the currently supported stores.
-
-    // Once the user clicks on one of those markers it will draw the shortest route towards that store.
-
-    // noticing that the displayed routes may look wonky at first, the function takes into
-    // consideration the direction of trafic
-
-    // We only support a few stores currently, but more will be added in the future.
-
+    //function that is triggered from the onclick listener on each imageview
     public void showPopup(View anchorView, float offsetX, float offsetY, Code code) {
 
         View popupView = getLayoutInflater().inflate(R.layout.map_popup, null);
@@ -167,7 +106,6 @@ public class StoreMap extends Activity {
 
         // Using location, the PopupWindow will be displayed right under anchorView
         //popupWindow.showAtLocation(anchorView, Gravity.NO_GRAVITY,location[0], location[1] + anchorView.getHeight());
-
         popupWindow.showAsDropDown(anchorView, (int) offsetX, (int) offsetY, Gravity.NO_GRAVITY);
     }
 }
